bdadf7b6babba5cfcceb3072015363c52faf2b24
rubens <rubesuaza1983@gmail.com>
2021-11-15 18:39:56 -0500

    creacion template inicio

--- home_page/urls.py
+++ home_page/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index,name='index'),
+    path('/servicio_1',views.service_1,name='servicio1'),
+    path('/who',views.whoAre,name='presentation')
    
 ]
--- home_page/views.py
+++ home_page/views.py
@@ -3,3 +3,9 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request,template_name='templates_home/index.html')
+
+def service_1(request):
+    return render(request,template_name='templates_home/servicio1.html')
+
+def whoAre(request):
+    return render(request,template_name='templates_home/presentation.html')
